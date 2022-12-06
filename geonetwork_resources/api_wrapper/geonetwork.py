bd6c76f0eace3f03983573aef3772c8259ec2395
HueyNemud <bertranddumenieu@gmail.com>
2022-12-06 15:41:08 +0100

    Implements 'logged in' sessions

--- geonetwork_resources/api_wrapper/geonetwork.py
+++ geonetwork_resources/api_wrapper/geonetwork.py
@@ -1,25 +1,25 @@
 import requests
 from . import config
 
-
-def get_cookies(client: requests.Session, https_verify: bool=True) -> requests.cookies.RequestsCookieJar:
-    """ Connects to the catalog to get session cookies.
+def get_cookies(session: requests.Session, https_verify: bool=True) -> requests.cookies.RequestsCookieJar:
+    """ Connect to Geonetwork API and return the cookies for the session passed is parameters.
     This function is useful to get a CSRF token that can be used in other requests.
     """
-    response = client.get(config.api_endpoint, headers={"accept": "application/json"}, verify=https_verify)
+    headers={"accept": "application/json"}
+    response = session.get(config.api_endpoint, headers=headers, verify=https_verify)
     response.raise_for_status()
     return response.cookies or {}
 
 
-def log_in(user: str, password: str, client: requests.Session=requests.Session()) -> str:
-    """ Connect to the catalog and log in with the credentials passed in arguments.
-    Returns a tuple2 containing the response object and the cookies generated for this session.
+def log_in(user: str, password: str, session: requests.Session=requests.Session()) -> str:
+    """ Connect to Geonetwork using the username and password in paramters.
+    Returns a requests.Session with a cookie holding a CSRF_TOKEN.
     """
-    cookies = get_cookies(client, False)
+    cookies = get_cookies(session, False)
     token = cookies.get("XSRF-TOKEN", None)
 
     if not token:
-        raise Exception("No XSRF-TOKEN returned.")
+        raise Exception("Could not get a XSRF-TOKEN.")
 
     opts = {
         "headers": {
@@ -30,6 +30,10 @@ def log_in(user: str, password: str, client: requests.Session=requests.Session()
         "cookies": cookies,
         "allow_redirects": True        
     }
-    response = client.get(config.api_endpoint, **opts)
+    response = session.get(config.api_endpoint, **opts)
     response.raise_for_status()
-    return response, cookies
+    
+    # Update session cookies with the cookie holding the CSRF TOKEN
+    cookies = requests.utils.dict_from_cookiejar(response.cookies)
+    session.cookies.update(cookies)
+    return session
