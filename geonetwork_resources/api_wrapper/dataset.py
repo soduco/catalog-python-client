ee9b68b14c7b577cdf8db62dac982188ef442970
mhersent <melvin.hersent@gmail.com>
2022-12-15 16:09:47 +0100

    doc added

--- geonetwork_resources/api_wrapper/dataset.py
+++ geonetwork_resources/api_wrapper/dataset.py
@@ -49,23 +49,47 @@ def upload(xml: ET.ElementTree, session: requests.Session=requests.Session()):
 
 # region UPDATE
 
-def update(uuid_list: List[UUID], edition_location: str, xml_patch: str, session: requests.Session=requests.Session()):
-    xpath = helpers.drop_leading_dot_in_xpath(edition_location)    
+def update(uuid_list: List[UUID],
+           edition_location: str,
+           xml_patch: str,
+           session: requests.Session=requests.Session()):
+    """
+    Call the batch_edit API endpoint in geonetwork.
+
+    :param session requests.Session: the http connexion
+    :param List[UUID] uuid_list: list of uuid to edit
+    :param str edition_location: xpath of the element to edit
+    :param str xml_patch: the xml element to add
+
+    Each xmlns must be declared.
+    The body request must look like this example, which add a source dataset:
+    "[{
+        \"xpath\":\"//mdb:resourceLineage/mrl:LI_Lineage\",
+        \"value\":
+            \"<mrl:source xmlns:mrl=\\\"http://standards.iso.org/iso/19115/-3/mrl/2.0\\\"
+            uuidref=\\\"e34f34cb-240a-469b-95f5-97075490505b\\\"/>\"
+    }]"
+    """
+
+    xpath = helpers.drop_leading_dot_in_xpath(edition_location)
     payload = json.dumps([{"xpath":xpath, "value": xml_patch}])
-    
+
     token = session.cookies.get_dict().get("XSRF-TOKEN")
     headers = {
         "X-XSRF-TOKEN": token,
         "accept": "application/json",
         "Content-Type": "application/json",
     }
-    
+
     params = {
         "uuids": uuid_list,
         "updateDateStamp": True
     }
-    
-    response = session.put(config.api_route_batchediting, headers=headers, params=params, data=payload)
+
+    response = session.put(config.api_route_batchediting,
+                           headers=headers,
+                           params=params,
+                           data=payload)
     response.raise_for_status()
     return response
 
