9103de3262d250c81839b3a6fc5b78d88c602e27
mhersent <melvin.hersent@gmail.com>
2022-12-19 15:24:30 +0100

    cli deleted dump of the yaml identifiers added

--- geonetwork_resources/api_wrapper/yaml_to_xml.py
+++ geonetwork_resources/api_wrapper/yaml_to_xml.py
@@ -1,48 +1,58 @@
 """Script that build xml records from yaml documents
 """
 
-import argparse
 import os
 import xml.etree.ElementTree as ET
-
+import csv
+import time
 import yaml
-from . import helpers, xml_composers
+from . import xml_composers
 
 
-def yaml_to_xml():
+def yaml_to_xml(inputfile: str, outpufile=None):
     """
-        Read yaml file
-        Build XML record
+        Read yaml file -> Build XML record
         Dump result in a xml file with "xml.etree.ElementTree.write()"
+        Dump csv with yaml identifiers and corresponding xml file
     """
-
-    parser = argparse.ArgumentParser(description='Upload records from a YAML file')
-    parser.add_argument("-i", dest="inputfile",
-                        required=True,
-                        help='input YAML file, with records to upload',
-                        metavar="INPUT FILE",
-                        type=lambda x: helpers.is_valid_file(parser, x))
-
-    args = parser.parse_args()
 
     # Loads a dataset definition from a YAML document
-    with args.inputfile as yaml_multidoc:
+    with open(inputfile, encoding='utf8') as yaml_multidoc:
+
+        doc_infos = []
 
         yaml_documents = list(yaml.load_all(yaml_multidoc, Loader=yaml.SafeLoader))
 
-        i = 1
-
         for yaml_doc in yaml_documents:
+
+            
             builder = xml_composers.IsoDocumentBuilder(yaml_doc)
             xml_tree = builder.compose_xml()
-            ET.indent(xml_tree)
+            ET.indent(xml_tree) # Beautify XML doc
 
             # we strip the input filename of it's path and extension
             # and add a number to form the xml filename
             # ex : fixtures/instance.yaml -> instance_01.xml
-            xml_filename = os.path.basename(args.inputfile.name)
-            xml_filename = os.path.splitext(args.inputfile.name)[0]
-            xml_filename = f"{xml_filename}_{i}.xml"
+            xml_filename = os.path.basename(inputfile)
+            xml_filename = os.path.splitext(inputfile)[0]
+            xml_filename = f"{yaml_doc['identifier']}.xml"
             xml_tree.write(xml_filename)
 
-            i+=1
+            doc_infos.append({"identifier": yaml_doc["identifier"], "xml_file": xml_filename})
+
+    fields = ['yaml_identifier', 'xml_file']
+
+    rows = []
+
+    for info in doc_infos:
+        rows.append([info['identifier'], info['xml_file']])
+
+
+    if outpufile is None:
+        outpufile = f'uuids_output_{time.strftime("%Y%m%d-%H%M%S")}.csv'
+
+    with open(outpufile, 'w', encoding='utf8') as file:
+        # using csv.writer method from CSV package
+        write = csv.writer(file)
+        write.writerow(fields)
+        write.writerows(rows)
